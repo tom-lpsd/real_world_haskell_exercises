b2658e2d995edde1ee63f44cf225c5bc956d1631
Tom Tsuruhara <tom.lpsd@gmail.com>
2008-12-21 18:22:14 +0900

    naive sort

---
+++ ch03/sortByLength.hs
@@ -0,0 +1,24 @@
+
+qsort :: (Ord a) => [a] -> [a]
+qsort [] = []
+qsort (x:xs) = qsort [l | l <- xs, l < x] ++
+               [x] ++
+               qsort [r | r <- xs, r >= x]
+
+bsort :: (Ord a) => [a] -> [a]
+bsort [] = []
+bsort (x:xs) = let (m, zs) = bubbleup x xs in m:bsort zs
+    where
+      bubbleup x [] = (x, [])
+      bubbleup x (y:ys)
+          | x > y = let (m, zs) = bubbleup y ys in (m, x:zs)
+          | otherwise = let (m, zs) = bubbleup x ys in (m, y:zs)
+
+sortByLength :: [[a]] -> [[a]]
+sortByLength [] = []
+sortByLength (x:xs) = let (m, zs) = bubbleup x xs in m:sortByLength zs
+    where
+      bubbleup x [] = (x, [])
+      bubbleup x (y:ys)
+          | length x > length y = let (m, zs) = bubbleup y ys in (m, x:zs)
+          | otherwise = let (m, zs) = bubbleup x ys in (m, y:zs)
