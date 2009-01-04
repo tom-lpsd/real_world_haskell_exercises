ee7fc6a6dbd4e75797cba86c60b919c28c4e6ae7
Tom Tsuruhara <tom.lpsd@gmail.com>
2009-01-04 15:13:48 +0900

    revise parse function using (>>?) operator.

--- ch10/PNM.hs
+++ ch10/PNM.hs
@@ -22,27 +22,19 @@ getBytes :: Int -> L.ByteString
 
 parseP5 :: L.ByteString -> Maybe (Greymap, L.ByteString)
 parseP5 s =
-    case matchHeader (L8.pack "P5") s of
-      Nothing -> Nothing
-      Just s1 ->
-          case getNat s1 of
-            Nothing -> Nothing
-            Just (width, s2) ->
-                case getNat (L8.dropWhile isSpace s2) of
-                  Nothing -> Nothing
-                  Just (height, s3) ->
-                      case getNat (L8.dropWhile isSpace s3) of
-                        Nothing -> Nothing
-                        Just (maxGrey, s4)
-                          | maxGrey > 255 -> Nothing
-                          | otherwise ->
-                              case getBytes 1 s4 of
-                                Nothing -> Nothing
-                                Just (_, s5) ->
-                                    case getBytes (width * height) s5 of
-                                      Nothing -> Nothing
-                                      Just (bitmap, s6) ->
-                                          Just (Greymap width height maxGrey bitmap, s6)
+    matchHeader (L8.pack "P5") s      >>?
+    \s -> skipSpace((), s)            >>?
+    (getNat . snd)                    >>?
+    skipSpace                         >>?
+    \(width, s) -> getNat s           >>?
+    skipSpace                         >>?
+    \(height, s) -> getNat s          >>?
+    \(maxGrey, s) -> getBytes 1 s     >>?
+    (getBytes (width * height) . snd) >>?
+    \(bitmap, s) -> Just (Greymap width height maxGrey bitmap, s)
+
+skipSpace :: (a, L.ByteString) -> Maybe (a, L.ByteString)
+skipSpace (a, s) = Just (a, L8.dropWhile isSpace s)
 
 matchHeader prefix str
     | prefix `L8.isPrefixOf` str
@@ -61,3 +53,7 @@ getBytes n str = let count            = fromIntegral n
                  in if L.length prefix < count
                     then Nothing
                     else Just both
+
+(>>?) :: Maybe a -> (a -> Maybe b) -> Maybe b
+Nothing >>? _ = Nothing
+Just v >>? f = f v
