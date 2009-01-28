ee0863575d6cb634658c6d8ad62dd4cbcf668a6f
Tom Tsuruhara <tom.lpsd@gmail.com>
2009-01-29 00:21:53 +0900

    examples in chapter 16

---
+++ ch16/HttpRequestParser.hs
@@ -0,0 +1,44 @@
+module HttpRequestParser
+    (
+      HttpRequest(..)
+    , Method(..)
+    , p_request
+    ) where
+
+import ApplicativeParsec
+import Numeric (readHex)
+import Control.Monad (liftM4)
+import System.IO (Handle)
+
+data Method = Get | Post deriving (Eq, Ord, Show)
+
+data HttpRequest = HttpRequest {
+      reqMethod :: Method
+    , reqURL :: String
+    , reqHeaders :: [(String, String)]
+    , reqBody :: Maybe String
+    } deriving (Eq, Show)
+
+p_request :: CharParser () HttpRequest
+p_request = q "GET" Get (pure Nothing)
+        <|> q "POST" Post (Just <$> many anyChar)
+    where q name ctor body = liftM4 HttpRequest req url p_headers body
+              where req = ctor <$ string name <* char ' '
+          url = optional (char '/') *>
+                manyTill notEOL (try $ string " HTTP/1." <* oneOf "01")
+                <* crlf
+
+p_headers :: CharParser st [(String, String)]
+p_headers = header `manyTill` crlf
+    where header = liftA2 (,) fieldName (char ':' *> spaces *> contents)
+          contents = liftA2 (++) (many1 notEOL <* crlf)
+                                 (continuation <|> pure [])
+          continuation = liftA2 (:) (' ' <$ many1 (oneOf " \t")) contents
+          fieldName = (:) <$> letter <*> many fieldChar
+          fieldChar = letter <|> digit <|> oneOf "-_"
+
+crlf :: CharParser st ()
+crlf = (() <$ string "\r\n") <|> (() <$ newline)
+
+notEOL :: CharParser st Char
+notEOL = noneOf "\r\n"
---
+++ ch16/csv2.hs
@@ -0,0 +1,9 @@
+import Text.ParserCombinators.Parsec
+
+csvFile = endBy line eol
+line = sepBy cell (char ',')
+cell = many (noneOf ",\n")
+eol = char '\n'
+
+parseCSV :: String -> Either ParseError [[String]]
+parseCSV input = parse csvFile "(unknown)" input
