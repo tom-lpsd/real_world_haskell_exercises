51744061400333b0078ef77ef66e4dd071db3f69
Tom Tsuruhara <tom.lpsd@gmail.com>
2009-03-03 23:45:16 +0900

    examples in chapter 18

---
+++ ch18/CountEntriesT.hs
@@ -0,0 +1,17 @@
+module CountEntriesT (listDirectory, countEntries) where
+
+import CountEntries (listDirectory)
+import System.Directory (doesDirectoryExist, getDirectoryContents)
+import System.FilePath ((</>))
+import Control.Monad (forM_, when)
+import Control.Monad.Trans (liftIO)
+import Control.Monad.Writer (WriterT, tell)
+
+countEntries :: FilePath -> WriterT [(FilePath, Int)] IO ()
+countEntries path = do
+  contents <- liftIO . listDirectory $ path
+  tell [(path, length contents)]
+  forM_ contents $ \name -> do
+                      let newName = path </> name
+                      isDir <- liftIO . doesDirectoryExist $ newName
+                      when isDir $ countEntries newName
